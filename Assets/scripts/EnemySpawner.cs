6ee50816088b19207175dda522906ddc1b074804
Wiktoryk <242486@edu.p.lodz.pl>
2024-12-01 21:50:26 +0100

    boss

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject bossPrefab;
     public int numberOfEnemies;
     public List<Transform> spawnPoints;
     public List<GameObject> enemies = new List<GameObject>();
@@ -15,9 +16,10 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-    public void Init(RoomData roomPosition)
+    public void Init(RoomData roomPosition, bool isBossRoom=false)
     {
         enemies.Clear();
+        spawnPoints.Clear();
         Vector3 currentRoomPositionV = roomPosition.RoomObject.transform.position;
         if (roomPosition.Position.x > 0)
         {
@@ -33,40 +35,72 @@ public class EnemySpawner : MonoBehaviour
         {
             currentRoomPositionV.y += 7;
         }
-        for (int i = 0; i < numberOfEnemies; i++)
+
+        if (isBossRoom)
         {
-            int xCoord = Random.Range((int)currentRoomPositionV.x-5, (int)currentRoomPositionV.x+5);
-            int yCoord = Random.Range((int)currentRoomPositionV.y-3, (int)currentRoomPositionV.y+3);
-            float snappedX = xCoord * 0.64f + 0.32f;
-            float snappedY = yCoord * 0.64f + 0.32f;
-            Transform spawnPoint = new GameObject().transform;
-            spawnPoint.position = new Vector3(snappedX, snappedY, 0.0f);
-            //spawnPoint.position = new Vector3(xCoord, yCoord, 0);
-            //ensure that the spawn point is not too close to the player or other spawn points
-            if (Vector3.Distance(spawnPoint.position, GameObject.Find("Player").transform.position) < 2)
+            bool correctSpawn = false;
+            while (!correctSpawn)
             {
-                i--;
-                Destroy(spawnPoint.gameObject);
-                continue;
-            }
-            bool tooClose = false;
-            foreach (Transform point in spawnPoints)
-            {
-                if (Vector3.Distance(spawnPoint.position, point.position) < 2)
+                int xCoord = Random.Range((int)currentRoomPositionV.x - 5, (int)currentRoomPositionV.x + 5);
+                int yCoord = Random.Range((int)currentRoomPositionV.y - 3, (int)currentRoomPositionV.y + 3);
+                float snappedX = xCoord * 0.64f + 0.32f;
+                float snappedY = yCoord * 0.64f + 0.32f;
+                Transform spawnPoint = new GameObject().transform;
+                spawnPoint.position = new Vector3(snappedX, snappedY, 0.0f);
+                correctSpawn = true;
+                if (Vector3.Distance(spawnPoint.position, GameObject.Find("Player").transform.position) < 2)
                 {
-                    tooClose = true;
-                    break;
+                    Destroy(spawnPoint.gameObject);
+                    correctSpawn = false;
+                }
+                if(correctSpawn)
+                {
+                    spawnPoints.Add(spawnPoint);
+                    SpawnBoss();
                 }
             }
-            if (tooClose)
-            {
-                i--;
-                Destroy(spawnPoint.gameObject);
-                continue;
-            }
-            spawnPoints.Add(spawnPoint);
         }
-        SpawnEnemies();
+        else
+        {
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                int xCoord = Random.Range((int)currentRoomPositionV.x - 5, (int)currentRoomPositionV.x + 5);
+                int yCoord = Random.Range((int)currentRoomPositionV.y - 3, (int)currentRoomPositionV.y + 3);
+                float snappedX = xCoord * 0.64f + 0.32f;
+                float snappedY = yCoord * 0.64f + 0.32f;
+                Transform spawnPoint = new GameObject().transform;
+                spawnPoint.position = new Vector3(snappedX, snappedY, 0.0f);
+                //spawnPoint.position = new Vector3(xCoord, yCoord, 0);
+                //ensure that the spawn point is not too close to the player or other spawn points
+                if (Vector3.Distance(spawnPoint.position, GameObject.Find("Player").transform.position) < 2)
+                {
+                    i--;
+                    Destroy(spawnPoint.gameObject);
+                    continue;
+                }
+
+                bool tooClose = false;
+                foreach (Transform point in spawnPoints)
+                {
+                    if (Vector3.Distance(spawnPoint.position, point.position) < 2)
+                    {
+                        tooClose = true;
+                        break;
+                    }
+                }
+
+                if (tooClose)
+                {
+                    i--;
+                    Destroy(spawnPoint.gameObject);
+                    continue;
+                }
+
+                spawnPoints.Add(spawnPoint);
+            }
+
+            SpawnEnemies();
+        }
     }
 
     void SpawnEnemies()
@@ -77,5 +111,12 @@ public class EnemySpawner : MonoBehaviour
             enemies.Add(enemy);
             enemy.SetActive(true);
         }
+    }
+    
+    void SpawnBoss()
+    {
+        GameObject enemy = Instantiate(bossPrefab, spawnPoints[0].position, Quaternion.identity);
+        enemies.Add(enemy);
+        enemy.SetActive(true);
     }
 }
